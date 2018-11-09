44c6bbd8536bb785a37bd7f297037c711157be45
forgottenfive <forgottenfive@outlook.com>
2018-11-09 14:37:00 -0600

    Update textToAES.py

--- textToAES.py
+++ textToAES.py
@@ -1,3 +1,8 @@
+# contributors: chum8
+# https://github.com/chum8
+# Evolve Security Academy Module 9 - homework assigment
+# Copyright (c) 2018-2019
+
 # import libraries
 import sys
 from Crypto.Cipher import AES
@@ -17,23 +22,26 @@ try:
         data = f.read()
 
 except:
-    print("Unable to open the file.  Legal file name not specified, or file does not exist.  Exiting.")
-    exit()
+    print("Unable to open the file.  Legal file name not specified, or file does not exist.\nExiting.")
+    sys.exit()
 
 # see if user is ready to proceed
 proceed = input("File '" + my_file +  "' will be encrypted using AES and output as '" + result_file + "'.  Type 'q' to cancel or ENTER to proceed: ")
 if proceed.lower() == 'q':
-    print("User canceled operation.  Exiting.")
-    exit()
+    print("User canceled operation.\nExiting.")
+    sys.exit()
 
 # encrpyt file
-print("Encrypting...")
-
-#try:
-#key = b'lP11dbkRATxS8LuG'
-key = Random.new().read(AES.block_size)
-iv = Random.new().read(AES.block_size)
-cipher = AES.new(key, AES.MODE_CFB, iv)
-result = iv + cipher.encrypt(data.encode('utf8'))
-print(result)
-#except:
+try:
+    key = Random.new().read(AES.block_size)
+    print("Encrypting using the following randomly generated key:\n%s" %key)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    result = iv + cipher.encrypt(data.encode('utf8'))
+    with open (result_file, 'w') as f:
+        f.write(str(result))
+    print("File successfully encrypted.")
+    print("Result written to '" + result_file + "'.\nExiting.")
+except:
+    print("Something went wrong during the encryption process.  No changes have been made.\nExiting.")
+sys.exit()
